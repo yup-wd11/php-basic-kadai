5664cbde17ee0a403a33362226ec06c0ad9820c6
yup-wd11 <seong10.rw.you@gmail.com>
2025-04-17 23:15:26 +0900

    kadai_007 課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>kadai_007</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 配列に値を代入する
+       $onion_date = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+       // 配列の値を出力する
+       print_r($onion_date);
+       ?>
+   </p>
+</body>
+
+</html>
